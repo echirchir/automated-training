e4ca64f85e1e77840374a5990054ce753ad55c91
Elisha Chirchir <elisha.chirchir@dej-technology.de>
2020-04-29 07:56:53 -0500

    Initial commit for project

---
+++ src/main/java/com/automated/training/AutomatedTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.automated.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTrainingApplication.class, args);
+	}
+
+}
