e59b4bf95ecc6e730d9c86c2305afe00df7e8238
Magda098 <64429234+Magda098@users.noreply.github.com>
2020-04-30 17:46:07 +0300

    adds user model

---
+++ src/main/java/com/automated/training/model/User.java
@@ -0,0 +1,21 @@
+package com.automated.training.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
